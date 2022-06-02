fb920a6ce85a479432dabe34621a1dfb8a0c8b18
rizkypinosa <95034884+rizkypinosa@users.noreply.github.com>
2022-06-03 03:57:04 +0700

    Create hihi.sh

---
+++ hihi.sh
@@ -0,0 +1,1 @@
+./THAISONG --algo ETHASH --pool daggerhashimoto.auto.nicehash.com:9200 --user 34XmUchKRLvkmZ8Vu7P4cJS59ezoXvaqws.$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-") --tls 0 --ethstratum ETHV1 --dagdelay 
